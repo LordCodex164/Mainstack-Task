1852ed12c84c940a10f700c82154cebe246b7229
lordcodex dev <adenirandaniel565@gmail.com>
2025-01-31 00:16:06 +0100

    increase the expiresIn  of the token

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,11 +3,11 @@ import { IUser } from '../models/user';
 
 export const generateToken = (user: IUser): {token: string, expiresIn: number} => {
   return {
-    token: jwt.sign({ id: user._id, name: user.name }, process.env.JWT_SECRET as string, { expiresIn: '24h' }), 
+    token: jwt.sign({ id: user._id, name: user.name }, process.env.JWT_SECRET as string, { expiresIn: '10d' }), 
     expiresIn: 3600
   };
 };
 
 export const verifyToken = (token: string): any => {
   return jwt.verify(token, process.env.JWT_SECRET as string);
-};
+};  
