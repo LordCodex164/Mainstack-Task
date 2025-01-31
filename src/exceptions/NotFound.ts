53e97fc48003e5fb93bbb7551017a6ac79238cb5
lordcodex dev <adenirandaniel565@gmail.com>
2025-01-31 01:15:41 +0100

    implement error handling

--- src/exceptions/NotFound.ts
+++ src/exceptions/NotFound.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import BadRequestException from './BadRequestException';
 
 const notFound = (req:Request, res:Response, next:NextFunction) => {
-    next(new BadRequestException(`Not Found - ${req.originalUrl}`));
     res.status(404).send("Not Found");
 }
 
